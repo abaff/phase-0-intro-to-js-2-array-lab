3b1122b2f487b3bb122bbbf699b1d76ba5d416e1
Allyson Baffour Owusu <boallyson@gmail.com>
2023-04-03 19:53:57 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats=["Milo","Otis","Garfield"]
+function  destructivelyAppendCat(Ralph){
+    cats.push(Ralph);
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+}
+function  destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+// function appendCat(Broom) {
+//     const copyOfCats = cats.slice();
+//     copyOfCats.push(Broom);
+//     return copyOfCats
+// }
+function appendCat(){
+    let newCats = [...cats, "Broom"];
+    return newCats
+}
+//  I use the spread operator because i wanted to add to the begining (non desctructive method)
+function prependCat(){
+    let newCats = ["Arnold", ...cats];
+    return newCats
+}
+// I need to use slice here because i want to remove an element nondestructively 
+function removeLastCat(){
+    let differentCat = cats.slice(0, cats.length-1)
+    return differentCat
+    
+}
+function removeFirstCat(){
+    let anotherCat=cats.slice(1)
+    return anotherCat
+}
